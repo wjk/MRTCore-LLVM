2ff07fe3da1039bfbb2df4d021b97c455baae5f1
William Kent <wjk011@gmail.com>
2025-10-04 14:30:27 -0400

    Rewrite target_win32_resources() function
    Also moved the MRM resource file back to its original name.

--- windres.cmake
+++ windres.cmake
@@ -1,20 +1,19 @@
-function(target_win32_resources target input)
-    cmake_parse_arguments(RES "" "" "ADDITIONAL_INPUTS;PREPROCESSOR_DEFINITIONS" ${ARGN})
+function(target_win32_resources target)
+    cmake_parse_arguments(RES "" "" "SOURCE_FILES;PREPROCESSOR_DEFINITIONS" ${ARGN})
+    if(NOT RES_SOURCE_FILES)
+        message(FATAL_ERROR "SOURCE_FILES must be specified when using target_win32_resources()")
+    endif()
 
     foreach(def ${RES_PREPROCESSOR_DEFINITIONS})
         list(APPEND flags --define=${def})
     endforeach()
 
-    get_filename_component(input_abs ${input} ABSOLUTE)
-    list(APPEND sources ${input_abs})
-    foreach(file ${RES_ADDITIONAL_INPUTS})
+    foreach(file ${RES_SOURCE_FILES})
         get_filename_component(file_abs ${file} ABSOLUTE)
         list(APPEND sources ${file_abs})
     endforeach()
 
-    get_filename_component(input_base ${input} NAME_WE)
-    set(output ${CMAKE_CURRENT_BINARY_DIR}/${input_base}.obj)
-
+    set(output ${CMAKE_CURRENT_BINARY_DIR}/resources.obj)
     add_custom_command(OUTPUT ${output}
         COMMAND ${WINDRES_EXECUTABLE} ${flags} --codepage=65001 --output=${output} ${sources}
         DEPENDS ${sources}
